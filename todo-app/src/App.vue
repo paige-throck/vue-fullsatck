98f47fca31d07c93171cf337f4d8871dff1776cf
Paige Throckmorton <path4942@gmail.com>
2018-02-28 09:23:43 -0600

    Pushing changes

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    
+  <div class='container'>
     <router-view/>
+  </div>  
   </div>
 </template>
 
--- todo-app/src/components/ToDoList.vue
+++ todo-app/src/components/ToDoList.vue
@@ -8,9 +8,30 @@
   </div>
 </template>
 
-<script type = "text/javascript" >
-
+<script>
 export default {
+  name: 'ToDoList',
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
 };
 </script>
 <style>
